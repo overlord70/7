b44e407a8052d94c75f4eda541aa1e36b61e91f4
overlord70 <s37776270@gmail.com>
2023-11-23 15:31:22 +0500

    hard

--- index.js
+++ index.js
@@ -1,45 +1,8 @@
-//1
-let your_age = prompt('How old are you?')
-function ask_age (age){
-    if(age >= 18){
-        alert('Welcome')
-    }else if(age === ''){
-        alert('Stupid')
-    } else{
-        alert('Get out of here')
+let num = Math.random()
+function create_id (random_num){ 
+    let arr = prompt('Your name')
+     alert( `${arr.charAt().toUpperCase()}` + '-'+ random_num.toString().slice(5,10))
+    
     }
-}
-ask_age (your_age)
-//2
-function find_max_name(name, name_second, name_third){
-    if(name.length > name_second.length && name.length >name_third.length ){
-        alert('The biggest one ' + name)
-    } else if(name_second.length > name.length && name_second.length > name_third.length){
-        alert('The biggest one ' + name_second)
-    } else if(name_third.length > name_second.length && name_third.length > name.length){
-        alert('The biggest one ' + name_third)
-    }
-}
-find_max_name('Alex', 'George', 'Michael')
-//3
-let overal = +prompt('How many things did you send?')
-let ask = confirm('Is it procent?')
-if(ask === false){
-    let remainder = +prompt('How many things were broken?')
-    function find_x (a,b){
-    let overal_procent = 100
-    let x = a * overal_procent / b
-    alert(overal_procent + '%' + ' = ' + `${b}` )
-    alert(`${x}` + '%' + ' = ' + `${a}`)
-    }
-    find_x(remainder, overal)
-} else{
-    let remainder_procent = +prompt('How many procent of them was broken?')
-    function find_y (c,d){
-        let overal_procent = 100
-        let y = c * d / overal_procent
-        alert( overal_procent + '%' + ' = ' + `${c}`)
-            alert(`${d}` + ' %' + ' = ' + `${y}`)
-    }
-    find_y(overal, remainder_procent)
-}
+create_id(num)
+
