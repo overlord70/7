81f7ef4142ae68435e899f3f111d7fece7c17b1c
overlord70 <s37776270@gmail.com>
2023-11-23 16:45:54 +0500

    copy

--- index.js
+++ index.js
@@ -6,3 +6,31 @@ function create_id (random_num){
     }
 create_id(num)
 
+// 2 
+function sortUsingForOf(inputArray) {
+    const sortedArray = [];
+  
+    for (const element of inputArray) {
+      if (sortedArray.length === 0) {
+        sortedArray.push(element);
+      } else {
+        let inserted = false;
+        for (let i = 0; i < sortedArray.length; i++) {
+          if (element < sortedArray[i]) {
+            sortedArray.splice(i, 0, element);
+            inserted = true;
+            break;
+          }
+        }
+        if (!inserted) {
+          sortedArray.push(element);
+        }
+      }
+    }
+  
+    return sortedArray;
+  }
+  const unsortedArray = [3, 1, 5, 2, 4];
+  const sortedResult = sortUsingForOf(unsortedArray);
+  console.log(sortedResult); 
+  
