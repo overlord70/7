10abe87087b7a1d782a5e69e847a5504823c41b2
overlord70 <s37776270@gmail.com>
2023-12-07 17:49:29 +0500

    new topic

--- index.js
+++ index.js
@@ -1,49 +1,21 @@
-const romains = (str) => {
-    const map = {
-        "I": 1,
-        "V": 5,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000
+let Hasan = document.querySelector('.Hasan')
+let Husen = document.querySelector('.Husen')
+
+Hasan.onclick = () => {
+    Husen.style.transition = '0.5s ease'
+    Husen.style.borderRadius = "50px"
+    Husen.style.background = 'green'
+    Husen.style.padding = '15px 15px 15px 15px'
+    if(Husen.style.background = 'green'){
+        Husen.style.marginLeft = '10px'
     }
-    let result = 0
-    let prev = 0
-    for(let i = str.length - 1; i>= 0; i--){
-        let curr = map[str[i]]
-        if(curr >= prev ){
-            result += curr
-        } else {
-            result-= curr
-        }
-        prev = curr
-    }
-    return result
 }
-const number_arabic = (number) => {
-    const map = {
-        "I": 1,
-        "V": 5,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000
+Husen.onclick = () => {
+    Hasan.style.transition = '0.5s ease'
+    Hasan.style.borderRadius = "50px"
+    Hasan.style.background = 'grey'
+    Hasan.style.padding = '15px 15px 15px 15px'
+    if(Hasan.style.background = 'grey'){
+        Hasan.style.marginRight = '10px'
     }
-    let roman = ''
-    for (let i = 0; i< number.length; i++) {
-        if (number >= pair[i]) {
-            roman += pair[i]
-        }else{
-            number -= pair[i]
-        }
-        
-    }
-    return roman
 }
-const your_num = ''
-const normal_num = romains(your_num)
-const result = number_arabic(normal_num)
-console.log(`${your_num}  ${normal_num}`)
-console.log(`${result}`)
