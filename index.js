e21f6e6c5b54a931376444067eb93efc41a51399
overlord70 <s37776270@gmail.com>
2023-12-12 17:10:12 +0500

    first

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let btn_0 = document.querySelector('.btn_1')
 let btn_2 = document.querySelector('.btn_2')
 let btn_3 = document.querySelector('.btn_3')
 let image = document.querySelector('.image')
-btn_0.onclick = () => {
-  
+let h3 = document.querySelector('.h3')
+btn_0.onmouseenter = () => {
+  h3.innerHTML = 'Color - black'
 }
