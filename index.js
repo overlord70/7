0ce36e54a0cb06a7ebe18a8b9f005531ebbde282
overlord70 <s37776270@gmail.com>
2023-12-12 16:39:52 +0500

    first

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-let box = document.querySelectorAll('.box')
-
-
-  box.forEach(item => {
-   item.onmouseenter = () => {
-    item.classList.add('active')
-   }
-   
-  }) 
+let btn_0 = document.querySelector('.btn_1')
+let btn_2 = document.querySelector('.btn_2')
+let btn_3 = document.querySelector('.btn_3')
+let image = document.querySelector('.image')
+btn_0.onclick = () => {
+  image.innerHTML = 
+}
