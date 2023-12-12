02d268d893b6a4bfcc425dd75118e78dbce9a370
overlord70 <s37776270@gmail.com>
2023-12-12 16:41:32 +0500

    commit

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ let btn_2 = document.querySelector('.btn_2')
 let btn_3 = document.querySelector('.btn_3')
 let image = document.querySelector('.image')
 btn_0.onclick = () => {
-  image.innerHTML = 
+  
 }
