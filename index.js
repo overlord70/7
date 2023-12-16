55151e1b3d1931f6aaad7296e4d631b71bc9b551
overlord70 <s37776270@gmail.com>
2023-12-16 16:39:08 +0500

    done

--- index.js
+++ index.js
@@ -1,18 +1,101 @@
-let price_view = document.querySelector('h1')
-let memory_btns = document.querySelectorAll('button')
-const original_price = 1000
-let prev = 0
-memory_btns.forEach((btn , idx) => {
-  btn.onclick = () => {
-    price_view.innerHTML = calculate(btn, original_price)
-
-    memory_btns[prev].classList.remove('active')
-    btn.classList.add('active')
-    prev = idx
+// let right = document.querySelector('.right')
+// let img = document.querySelector('.photos_img')
+// let btr = document.querySelector('.btr')
+// let photos = document.querySelector('.photos')
+// if(right.innerText === '>'){
+//   btr.onclick = () => {
+//     img.setAttribute('src', './img/iphone-15-pro-model-unselect-gallery-2-202309_GEO_US.jpg')
+//     right.innerHTML = '<'
+//     btr.classList.add('left')
+//     if(right.innerText === '<'){
+//       btr.onclick = () => {
+//         img.setAttribute('src','./img/iphone-15-pro-model-unselect-gallery-1-202309.jpg')
+//         right.innerHTML = '>'
+//         btr.classList.remove('left') 
+//     }
+//     }
+//   }
+// }
+// let color_choose_1 = document.querySelector('.color_choose_1')
+// let color_choose_2 = document.querySelector('.color_choose_2')
+// let color_choose_3 = document.querySelector('.color_choose_3')
+// let color_choose_4 = document.querySelector('.color_choose_4')
+// color_choose_1.onclick = () => {
+// img.setAttribute('src', './img/iphone_1.jpg')
+// img.classList.add('active')
+// }
+// color_choose_2.onclick = () => {
+//   img.setAttribute('src', './img/iphone_blue_5.jpg')
+//   img.classList.add('active')
+//   }
+//   color_choose_3.onclick = () => {
+//     img.setAttribute('src', './img/iphone_black_13.jpg')
+//     img.classList.add('active')
+//     }
+//     color_choose_4.onclick = () => {
+//       img.setAttribute('src', './img/iphone_white_9.jpg')
+//       img.classList.add('active')
+//     }
+//     let h3 = document.querySelector('.h3')
+//     color_choose_1.onmouseenter = () => {
+//       h3.innerHTML = 'Color - natural'
+//     }
+//     color_choose_1.onmouseleave = () => {
+//       h3.innerHTML = 'Color'
+//     }
+//     color_choose_2.onmouseenter = () => {
+//       h3.innerHTML = 'Color - blue titan'
+//     }
+//     color_choose_2.onmouseleave = () => {
+//       h3.innerHTML = 'Color'
+//     }
+//     color_choose_3.onmouseenter = () => {
+//       h3.innerHTML = 'Color - black titan'
+//     }
+//     color_choose_3.onmouseleave = () => {
+//       h3.innerHTML = 'Color'
+//     }
+//     color_choose_4.onmouseenter = () => {
+//       h3.innerHTML = 'Color - white titan'
+//       h3.style.transition = '5s ease'
+//     }
+//     color_choose_4.onmouseleave = () => {
+//       h3.innerHTML = 'Color'
+//     }
+    let none = document.querySelector('.none')
+    let block = document.querySelector('.block')
+    let down = document.querySelector('.down')
+    
+    
+      block.onclick = () => {
+        if(down.innerText === '⬇'){
+          none.style.display = 'block'
+           down.innerHTML = '⬆'
+        } else {
+          none.style.display = 'none'
+           down.innerHTML = '⬇'
+        }
+        }
+let p = document.querySelector('.click_p')
+let nav = document.querySelector('.nav')
+let up_one = document.querySelector('.up_one')
+p.onclick = () => {
+  if( up_one.innerText === '⬆'){
+    up_one.innerHTML = '⬇'
+    nav.style.display = 'none'
+  } else {
+    up_one.innerHTML = '⬆'
+    nav.style.display = 'block'
   }
-})
-function calculate (btn, original_price) {
-  let add_price = btn.firstElementChild.innerHTML.split('+')
-  add_price = +add_price[add_price.length - 1]
-  return(original_price + add_price)
 }
+     let ssilka = document.querySelector('.ssilka')
+     let modal = document.querySelector('.modal')
+     let back = document.querySelector('.back')
+     let x = document.querySelector('.x')
+    ssilka.onclick = () => {
+        modal.style.display = 'block'
+    }
+    x.onclick = () => {
+      modal.style.display = 'none'
+    }
+    
