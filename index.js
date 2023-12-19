eb92a8a6a2425c381cfef120157a8d9b0d87bda8
overlord70 <s37776270@gmail.com>
2023-12-19 15:28:44 +0500

    done

--- index.js
+++ index.js
@@ -1,101 +1,103 @@
-// let right = document.querySelector('.right')
-// let img = document.querySelector('.photos_img')
-// let btr = document.querySelector('.btr')
-// let photos = document.querySelector('.photos')
-// if(right.innerText === '>'){
-//   btr.onclick = () => {
-//     img.setAttribute('src', './img/iphone-15-pro-model-unselect-gallery-2-202309_GEO_US.jpg')
-//     right.innerHTML = '<'
-//     btr.classList.add('left')
-//     if(right.innerText === '<'){
-//       btr.onclick = () => {
-//         img.setAttribute('src','./img/iphone-15-pro-model-unselect-gallery-1-202309.jpg')
-//         right.innerHTML = '>'
-//         btr.classList.remove('left') 
-//     }
-//     }
-//   }
-// }
-// let color_choose_1 = document.querySelector('.color_choose_1')
-// let color_choose_2 = document.querySelector('.color_choose_2')
-// let color_choose_3 = document.querySelector('.color_choose_3')
-// let color_choose_4 = document.querySelector('.color_choose_4')
-// color_choose_1.onclick = () => {
-// img.setAttribute('src', './img/iphone_1.jpg')
-// img.classList.add('active')
-// }
-// color_choose_2.onclick = () => {
-//   img.setAttribute('src', './img/iphone_blue_5.jpg')
-//   img.classList.add('active')
-//   }
-//   color_choose_3.onclick = () => {
-//     img.setAttribute('src', './img/iphone_black_13.jpg')
-//     img.classList.add('active')
-//     }
-//     color_choose_4.onclick = () => {
-//       img.setAttribute('src', './img/iphone_white_9.jpg')
-//       img.classList.add('active')
-//     }
-//     let h3 = document.querySelector('.h3')
-//     color_choose_1.onmouseenter = () => {
-//       h3.innerHTML = 'Color - natural'
-//     }
-//     color_choose_1.onmouseleave = () => {
-//       h3.innerHTML = 'Color'
-//     }
-//     color_choose_2.onmouseenter = () => {
-//       h3.innerHTML = 'Color - blue titan'
-//     }
-//     color_choose_2.onmouseleave = () => {
-//       h3.innerHTML = 'Color'
-//     }
-//     color_choose_3.onmouseenter = () => {
-//       h3.innerHTML = 'Color - black titan'
-//     }
-//     color_choose_3.onmouseleave = () => {
-//       h3.innerHTML = 'Color'
-//     }
-//     color_choose_4.onmouseenter = () => {
-//       h3.innerHTML = 'Color - white titan'
-//       h3.style.transition = '5s ease'
-//     }
-//     color_choose_4.onmouseleave = () => {
-//       h3.innerHTML = 'Color'
-//     }
-    let none = document.querySelector('.none')
-    let block = document.querySelector('.block')
-    let down = document.querySelector('.down')
-    
-    
-      block.onclick = () => {
-        if(down.innerText === '⬇'){
-          none.style.display = 'block'
-           down.innerHTML = '⬆'
-        } else {
-          none.style.display = 'none'
-           down.innerHTML = '⬇'
-        }
-        }
-let p = document.querySelector('.click_p')
-let nav = document.querySelector('.nav')
-let up_one = document.querySelector('.up_one')
-p.onclick = () => {
-  if( up_one.innerText === '⬆'){
-    up_one.innerHTML = '⬇'
-    nav.style.display = 'none'
-  } else {
-    up_one.innerHTML = '⬆'
-    nav.style.display = 'block'
-  }
+let temperature = document.querySelector('#temperature')
+let one = document.querySelector('#one')
+let two = document.querySelector('#two')
+let temp = +temperature.innerHTML.slice(0,2)
+let air_conditioner = document.querySelector('#air_conditioner')
+one.onclick = () => {
+    temp++ 
+    if(temp >= 15) {
+        air_conditioner.innerHTML = 'Кондиционер'
+    }
+    temperature.innerHTML = temp + '°'
 }
-     let ssilka = document.querySelector('.ssilka')
-     let modal = document.querySelector('.modal')
-     let back = document.querySelector('.back')
-     let x = document.querySelector('.x')
-    ssilka.onclick = () => {
-        modal.style.display = 'block'
+two.onclick = () => {
+    temp--
+    if(temp < 15) {
+    air_conditioner.innerHTML = 'Печка'
+    } 
+    temperature.innerHTML = temp + '°'
+}
+
+let three = document.querySelector('#three')
+let four = document.querySelector('#four')
+let hm = document.querySelector('#hm')
+let p = +hm.innerHTML.slice(0,2)  
+let supply = document.querySelector('#supply')
+let t = +supply.innerHTML.slice(0,3)
+three.onclick = () => {
+ if(p < 80){
+     p += 5 
+     t -= 10
+     supply.innerHTML = t + 'км'
+ } else {
+    p = 80
+ }
+ hm.innerHTML = p + ' км/ч'
+}
+four.onclick = () => {
+    if(p > 20){
+        p -= 5
+        t += 10
+        supply.innerHTML = t + 'км'
+    } else {
+        p = 20
     }
-    x.onclick = () => {
-      modal.style.display = 'none'
-    }
-    
+    hm.innerHTML = p + ' км/ч'
+   }
+let imgs = document.querySelectorAll('.wheel')
+let five = document.querySelector('#five')
+let nineteen = document.querySelector('#nineteen')
+let six = document.querySelector('#six')
+five.onclick = () => {
+    nineteen.innerHTML = 21
+    imgs.forEach(img => {
+        img.setAttribute('src', './img/cold-removebg-preview.png')
+    })
+}
+six.onclick = () => {
+    nineteen.innerHTML = 19
+    imgs.forEach(img => {
+        img.setAttribute('src', './img/hot-removebg-preview.png')
+    })
+}
+let checked = document.querySelector('#checked')
+let section = document.querySelector('.Tesla')
+let section_2 = document.querySelector('.Tesla_2')
+let click = document.querySelector('#click')
+let img_salon = document.querySelector('.img_salon')
+checked.onclick = () => {
+  section.style.display = 'none'
+  section_2.style.display = 'block'
+}
+click.onclick = () => {
+    section.style.display = 'block'
+    section_2.style.display = 'none'
+}
+let black = document.querySelector('.black')
+let colour = document.querySelector('.colour')
+let white = document.querySelector('.white')
+white.onclick = () => {
+    img_salon.setAttribute('src', './img/image 9.png')
+    img_salon.style.width = '500%'
+    img_salon.classList.add('active')
+}
+black.onclick = () => {
+    img_salon.setAttribute('src', './img/image 1.png')
+    img_salon.style.width = '500%'
+    img_salon.classList.add('active')
+}
+colour.onclick = () => {
+    img_salon.setAttribute('src', './img/image 8.png')
+    img_salon.style.width = '500%'
+    img_salon.classList.add('active')
+}
+let tiggle = document.querySelector('#tiggle')
+tiggle.onclick = () => {
+   if(tiggle.innerHTML = tiggle.checked){
+    t -= 20
+    supply.innerHTML = t + 'км'
+   } else {
+    t += 20
+    supply.innerHTML = t + 'км'
+   }
+}
