1d139d9387a902cb778a9596f7c80a7b678e7848
overlord70 <s37776270@gmail.com>
2024-01-06 19:03:56 +0500

    class work

--- index.js
+++ index.js
@@ -1,41 +1,21 @@
-let inputs = document.querySelectorAll('.word')
+let inps = document.querySelectorAll('input')
 let btn = document.querySelector('button')
 let number = document.querySelector('.number')
-inputs.forEach(inp => {
+const patterns = {
+  name: /^[a-z ,.'-]+$/i,
+  surname: /^[a-z ,.'-]+$/i,
+  phone_number: /^9989[012345789][0-9]{7}$/,
+  email: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+  age:/^\S[0-9]{0,3}$/
+}
+inps.forEach(inp => {
   inp.onkeyup = () => {
-    if(/^[a-z ,.'-]+$/i.test(inp.value) === true){
-      inp.style.border = '1px solid gray'
-      btn.style.background = '#4200FF'
-      btn.style.border = '2px solid #4200FF'
-    } else {
-      inp.style.border = '2px solid red'
-      btn.style.background = 'red'
-      btn.style.border = '2px solid red'
-    }
+      let reg = patterns[inp.name]
+      if(reg.test(inp.value)) {
+          inp.parentElement.classList.remove('error-field')
+      } else {
+          inp.parentElement.classList.add('error-field')
+      }
+
   }
 })
-let important = document.querySelectorAll('.important input')
-btn.onclick = () => {
-  important.forEach(inp => {
-    if (inp.value === '') {
-      inp.style.border = '2px solid red'
-      btn.style.border = '2px solid red'
-      btn.style.background = 'red'
-    } else {
-      inp.style.border = '1px solid gray'
-      btn.style.border = '2px solid #4200FF'
-      btn.style.background = '#4200FF'
-    }
-  })
-}
-number.onkeyup = () => {
-  if(/^9989[012345789][0-9]{7}$/.test(number.value) === true){
-    number.style.border = '1px solid gray'
-      btn.style.background = '#4200FF'
-      btn.style.border = '2px solid #4200FF'
-   } else {
-    number.style.border = '2px solid red'
-      btn.style.background = 'red'
-      btn.style.border = '2px solid red'
-   }
-}
