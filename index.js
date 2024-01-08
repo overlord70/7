52a5467f9d4d832bb5bae5574ed36bdd3eb2b66c
overlord70 <s37776270@gmail.com>
2024-01-08 06:38:40 +0500

    done 50%

---
+++ index.js
@@ -0,0 +1,10 @@
+let btn_one = document.querySelector('.btns .one')
+let btn_two = document.querySelector('.btns .two')
+let btn_three = document.querySelector('.btns .three')
+let one = document.querySelector('#one')
+let two = document.querySelector('#two')
+let three = document.querySelector('#three')
+let four = document.querySelector('#four')
+one.onmouseenter = () => {
+    
+}
